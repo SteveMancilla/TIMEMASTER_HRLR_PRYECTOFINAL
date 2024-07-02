29a5546320dbc3588226f61a6a56a3189a0a0704
steven <72620878@continental.edu.pe>
2024-07-02 11:15:17 -0500

    Actualizado

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #NO BORRAR
 import sys
 from PyQt5.QtWidgets import QApplication
-from ventanaInteractiva import Ui_MainWindow
+from login import Ui_MainWindow
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
     window = Ui_MainWindow()
     window.show()
-    app.exec(app.exec())
+    app.exec()
