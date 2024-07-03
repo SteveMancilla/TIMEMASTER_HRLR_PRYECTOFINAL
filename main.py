180fd8aace363b19160b7e41bde6d956b3974e80
steven <72620878@continental.edu.pe>
2024-07-02 20:00:48 -0500

    Pomodoro interfaz menu desplegable

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #NO BORRAR -nnn
 import sys
 from PyQt5.QtWidgets import QApplication
-from ventanaInteractiva import Ui_MainWindow
+from Pomodoro import Ui_windowPomodoro
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
-    window = Ui_MainWindow()
+    window = Ui_windowPomodoro()
     window.show()
     app.exec()
